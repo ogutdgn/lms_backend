51b87c43ada8999d24f3bcee9e2b48ce709e328d
doan <ogutdgn@gmail.com>
2024-07-11 16:32:10 +0300

    .

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json())
 app.use(require('./src/middlewares/authentication'))
 
 // Run Logger:
-app.use(require('./src/middlewares/logger'))
+// app.use(require('./src/middlewares/logger'))
 
 // res.getModelList():
 app.use(require('./src/middlewares/queryHandler'))
